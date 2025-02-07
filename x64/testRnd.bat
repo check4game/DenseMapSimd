44b1a58e8093c55b3e5194c8f42b1d44b54aece2
check4game <maxim.amsd@gmail.com>
2025-02-07 17:20:42 +0300

    Add result logs  for 1015  & test .bat

---
+++ x64/testRnd.bat
@@ -0,0 +1,7 @@
+DenseMapSimd.exe rnd 128
+DenseMapSimd.exe run -ht -64
+DenseMapSimdF1.exe run -ht -64
+DenseMapSimdF2.exe run -ht -64
+DenseMapSimd.exe run -ht -32
+DenseMapSimdF1.exe run -ht -32
+DenseMapSimdF2.exe run -ht -32
